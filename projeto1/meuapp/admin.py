fe02f407328927b190fffb58521616f15ada515a
MATHEUS VINICIUS RAMOS FERREIRA <vinicius.matheus@aluno.senac.rio>
2025-10-03 21:22:59 -0300

    a

--- projeto1/meuapp/admin.py
+++ projeto1/meuapp/admin.py
@@ -3,7 +3,7 @@ from .models import Cliente
 # Register your models here.
 
 @admin.register(Cliente)
-class tabelaadmin(admin.ModelAdmin):
+class tabela_de_clientes(admin.ModelAdmin):
     list_display=('nome','email')
     
     search_fields=('nome','email')
